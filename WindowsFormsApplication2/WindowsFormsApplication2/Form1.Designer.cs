e4db603e4d2e03e2612812ca8b7abf208a5926c0
Michael Salivon <michael@ppcinj.com>
2017-01-27 19:09:20 +0100

    Reduce code in tictactoe

--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -47,7 +47,6 @@
             this.button1.Size = new System.Drawing.Size(75, 75);
             this.button1.TabIndex = 0;
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button3
             // 
@@ -57,7 +56,6 @@
             this.button3.Size = new System.Drawing.Size(75, 75);
             this.button3.TabIndex = 1;
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -67,7 +65,6 @@
             this.button2.Size = new System.Drawing.Size(75, 75);
             this.button2.TabIndex = 2;
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button5
             // 
@@ -77,7 +74,6 @@
             this.button5.Size = new System.Drawing.Size(75, 75);
             this.button5.TabIndex = 5;
             this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -87,7 +83,6 @@
             this.button6.Size = new System.Drawing.Size(75, 75);
             this.button6.TabIndex = 4;
             this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button4
             // 
@@ -97,7 +92,6 @@
             this.button4.Size = new System.Drawing.Size(75, 75);
             this.button4.TabIndex = 3;
             this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button8
             // 
@@ -107,7 +101,6 @@
             this.button8.Size = new System.Drawing.Size(75, 75);
             this.button8.TabIndex = 8;
             this.button8.UseVisualStyleBackColor = true;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -117,7 +110,6 @@
             this.button9.Size = new System.Drawing.Size(75, 75);
             this.button9.TabIndex = 7;
             this.button9.UseVisualStyleBackColor = true;
-            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button7
             // 
@@ -127,7 +119,6 @@
             this.button7.Size = new System.Drawing.Size(75, 75);
             this.button7.TabIndex = 6;
             this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // Form1
             // 
@@ -144,8 +135,7 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "TicTacToe";
             this.ResumeLayout(false);
 
         }
--- WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -1,221 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication2
 {
     public partial class Form1 : Form
     {
+        private int _id;
+
         public Form1()
         {
             InitializeComponent();
+            foreach (var button in Controls.OfType<Button>())
+            {
+                button.Click += delegate(object sender, EventArgs args)
+                {
+                    var clickedButton = (Button) sender;
+                    var ost = _id % 2;
+                    clickedButton.Text = (ost == 0 ? "X" : "O");
+                    clickedButton.Enabled = false;
+                    ProverkaEnd();
+                    _id++;
+                };
+            }
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        public int id = 0;
         public void ProverkaEnd()
         {
             if(button1.Text=="X" && button2.Text=="X" && button3.Text == "X" || button4.Text=="X" && button5.Text=="X" && button6.Text == "X" || button7.Text == "X" && button8.Text == "X" && button9.Text == "X" || button1.Text == "X" && button5.Text == "X" && button9.Text == "X" || button7.Text == "X" && button5.Text == "X" && button3.Text == "X" || button1.Text == "X" && button4.Text == "X" && button7.Text == "X" || button2.Text == "X" && button5.Text == "X" && button8.Text == "X" || button3.Text == "X" && button6.Text == "X" && button9.Text == "X")
             {
                 MessageBox.Show("Выиграли Х");
-                button1.Text = "";
-                button1.Enabled = true;
-                button2.Text = "";
-                button2.Enabled = true;
-                button3.Text = "";
-                button3.Enabled = true;
-                button4.Text = "";
-                button4.Enabled = true;
-                button5.Text = "";
-                button5.Enabled = true;
-                button6.Text = "";
-                button6.Enabled = true;
-                button7.Text = "";
-                button7.Enabled = true;
-                button8.Text = "";
-                button8.Enabled = true;
-                button9.Text = "";
-                button9.Enabled = true;
+                ResetField();
             }
             if (button1.Text == "O" && button2.Text == "O" && button3.Text == "O" || button4.Text == "O" && button5.Text == "O" && button6.Text == "O" || button7.Text == "O" && button8.Text == "O" && button9.Text == "O" || button1.Text == "O" && button5.Text == "O" && button9.Text == "O" || button7.Text == "O" && button5.Text == "O" && button3.Text == "O" || button1.Text == "O" && button4.Text == "O" && button7.Text == "O" || button2.Text == "O" && button5.Text == "O" && button8.Text == "O" || button3.Text == "O" && button6.Text == "O" && button9.Text == "O")
             {
                 MessageBox.Show("Выиграли O");
-                button1.Text = "";
-                button1.Enabled = true;
-                button2.Text = "";
-                button2.Enabled = true;
-                button3.Text = "";
-                button3.Enabled = true;
-                button4.Text = "";
-                button4.Enabled = true;
-                button5.Text = "";
-                button5.Enabled = true;
-                button6.Text = "";
-                button6.Enabled = true;
-                button7.Text = "";
-                button7.Enabled = true;
-                button8.Text = "";
-                button8.Enabled = true;
-                button9.Text = "";
-                button9.Enabled = true;
+                ResetField();
             }
         }
-        
-           
-        
 
-        private void button1_Click(object sender, EventArgs e)
+        private void ResetField()
         {
-            int ost = id % 2;
-            if (ost == 0)
+            foreach (var button in Controls.OfType<Button>())
             {
-                button1.Text = "X";
+                button.Text = "";
+                button.Enabled = true;
             }
-            else
-            {
-                button1.Text = "O";
-            }
-            ProverkaEnd();
-            button1.Enabled = false;
-            id++;
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            int ost = id % 2;
-            if (ost == 0)
-            {
-                button2.Text = "X";
-            }
-            else
-            {
-                button2.Text = "O";
-            }
-            button2.Enabled = false;
-            ProverkaEnd();
-            id++;
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            int ost = id % 2;
-            if (ost == 0)
-            {
-                button3.Text = "X";
-            }
-            else
-            {
-                button3.Text = "O";
-            }
-            button3.Enabled = false;
-            ProverkaEnd();
-            id++;
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            int ost = id % 2;
-            if (ost == 0)
-            {
-                button4.Text = "X";
-            }
-            else
-            {
-                button4.Text = "O";
-            }
-            button4.Enabled = false;
-            ProverkaEnd();
-            id++;
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            int ost = id % 2;
-            if (ost == 0)
-            {
-                button5.Text = "X";
-            }
-            else
-            {
-                button5.Text = "O";
-            }
-            button5.Enabled = false;
-            ProverkaEnd();
-            id++;
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-            int ost = id % 2;
-            if (ost == 0)
-            {
-                button6.Text = "X";
-            }
-            else
-            {
-                button6.Text = "O";
-            }
-            button6.Enabled = false;
-            ProverkaEnd();
-            id++;
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            int ost = id % 2;
-            if (ost == 0)
-            {
-                button7.Text = "X";
-            }
-            else
-            {
-                button7.Text = "O";
-            }
-            button7.Enabled = false;
-            ProverkaEnd();
-            id++;
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-            int ost = id % 2;
-            if (ost == 0)
-            {
-                button8.Text = "X";
-            }
-            else
-            {
-                button8.Text = "O";
-            }
-            button8.Enabled = false;
-            ProverkaEnd();
-            id++;
-        }
-
-        private void button9_Click(object sender, EventArgs e)
-        {
-            int ost = id % 2;
-            if (ost == 0)
-            {
-                button9.Text = "X";
-            }
-            else
-            {
-                button9.Text = "O";
-            }
-            button9.Enabled = false;
-            ProverkaEnd();
-            id++;
         }
     }
 }
